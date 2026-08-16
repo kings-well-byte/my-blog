482814509100eb3dc535e85a9fd8ffc1131f2a6f
kings-well-byte <1573049538@qq.com>
2026-08-16 16:26:57 +0800

    update: 全面个性化配置

--- my-blog-manager/siteConfig.ts
+++ my-blog-manager/siteConfig.ts
@@ -31,18 +31,18 @@ faviconUrl: "https://raw.githubusercontent.com/kings-well-byte/images/main/2eccd
   photoWallImage: "https://bu.dusays.com/2026/03/24/69c1e38b4c370.jpg",
   cloudMusicIds: ["1809646618", "3361076230", "1859390262"],
   social: {
-    github: "https://github.com/heiehiehi",
-    gitee: "https://github.com/heiehiehi",
-    google: "mailto:bilibiliwuwuwu@gmail.com",
-    email: "1124533793@qq.com",
-    qq: "1124533793",
-    wechat: "XingHuisama",
+    github: "https://github.com/kings-well-byte",
+    gitee: "",
+    google: "1573049538@gmail.com",
+    email: "1573049538@qq.com",
+    qq: "1573049538",
+    wechat: "怀桑",
   },
   counts: {
     photos: 128, // 照片墙数量可以手动写死或动态计算
   },
-  chatterTitle: "云端杂谈", // 你可以改成任何你喜欢的名字
-  chatterDescription: "代码、学术、提瓦特与泰拉大陆的碎片记录",
+  chatterTitle: "黑客历程", // 你可以改成任何你喜欢的名字
+  chatterDescription: "网络安全学习笔记与 CTF 刷题记录",
 
   // 👇 【新增】：图床核心配置 (PicBed Configuration)
   picBedName: "图床",
@@ -50,7 +50,7 @@ faviconUrl: "https://raw.githubusercontent.com/kings-well-byte/images/main/2eccd
   picBedToken: "", // 留空，等你能在后台填入并覆写
 
   // 👇 【新增】：全局背景弹幕配置
-  danmakuList: ["在干嘛呢？", "有笨蛋嘛？", "前方高能反应！", "GROMACS 跑起来了吗？", "MD 模拟什么时候才能出图啊", "Graph Neural Networks 炼丹中...", "BUG 修复进度 99%", "今天背单词了吗？", "Tailwind CSS 拯救前端", "写算法中", "睡大觉中", "到底在干嘛？"],
+  danmakuList: ["正在学习网络安全","CTF冲鸭","红队奋斗中，励志冲上'紫队'宝座","渗透测试中...."],
   gitalkConfig: {
     clientID: "",
     clientSecret: "",
@@ -58,7 +58,7 @@ faviconUrl: "https://raw.githubusercontent.com/kings-well-byte/images/main/2eccd
     owner: "",
     admin: [""],
   },
-  buildDate: "2026-03-23T00:00:00", // 建站日期
+  buildDate: "2026-08-16T00:00:00", // 建站日期
   footerBadges: [{"name": "Next.js 15", "color": "text-sky-500", "svg": "<path d=\"M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z\"/>"}, {"name": "React 19", "color": "text-cyan-400", "svg": "<path d=\"M12 22.6l-9.8-5.6V5.6L12 0l9.8 5.6v11.4l-9.8 5.6zm-8.2-6.5l8.2 4.7 8.2-4.7V7.5L12 2.8 3.8 7.5v8.6z\"/>"}, {"name": "Tailwind 4", "color": "text-teal-400", "svg": "<path d=\"M12.001,4.8c-3.2,0-5.2,1.6-6,4.8c1.2-1.6,2.6-2.2,4.2-1.8c0.913,0.228,1.565,0.89,2.288,1.624C13.666,10.618,15.027,12,18.001,12 c3.2,0,5.2-1.6,6-4.8c-1.2,1.6-2.6,2.2-4.2,1.8c-0.913-0.228-1.565-0.89-2.288-1.624C16.337,6.182,14.976,4.8,12.001,4.8z M6.001,12c-3.2,0-5.2,1.6-6,4.8c1.2-1.6,2.6-2.2,4.2-1.8c0.913,0.228,1.565,0.89,2.288,1.624c1.177,1.194,2.538,2.576,5.512,2.576 c3.2,0,5.2-1.6,6-4.8c-1.2,1.6-2.6,2.2-4.2,1.8c-0.913-0.228-1.565-0.89-2.288-1.624C10.337,13.382,8.976,12,6.001,12z\"/>"}],
   icpConfig: {
     name: "萌ICP备 20260240号",
@@ -70,6 +70,5 @@ faviconUrl: "https://raw.githubusercontent.com/kings-well-byte/images/main/2eccd
     maxOutputTokens: 150,
     temperature: 0.85,
   },
-  friendLinkApplyFormat: "名称：XingHuiSamaの宝藏之地\n简介：今天我也要学习吗\n链接：https://www.xinghuisama.top\n头像：https://bu.dusays.com/2026/03/24/69c1e38ac1846.jpg",
-  enableLevelSystem: true,
+  friendLinkApplyFormat: "名称：-L.の博客\n简介：网络安全学习中\n链接：https://my-blog-blog-ea59.vercel.app\n头像：https://raw.githubusercontent.com/kings-well-byte/images/main/2eccd9731f2f24a697c4c7cc4b0399c1.jpg",
 };
